836dfe0197f546fcb274284edff8e4e1474ec0d1
ivanfslee <ivanfslee@gmail.com>
2020-05-22 00:29:14 -0700

    Added quick node server. Need to add some postman notes

--- introNotes.js
+++ introNotes.js
@@ -32,3 +32,22 @@ HTTP status codes:
     200 OK
     404 Not found
 */
+
+const http = require('http'); //http comes with node, so we don't have to 'npm install' it
+
+//http has a method called 'createServer()' that takes a function
+    //the function takes a req object and res object
+const server = http.createServer((req, res) => {
+    
+    //Using 'destructuring' JavaScript syntax to
+        //pull out req.headers / req.url / req.method from req object
+    const { headers, url, method } = req;
+
+    console.log(headers, url, method);
+    
+    //res.end() will set status code as 200 (as long as there are no errors)
+    res.end(); 
+});
+
+const PORT = 5000
+server.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`));
