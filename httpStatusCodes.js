b3d2282c4b9631403c7c7b96fe520fa4a370cf28
ivanfslee <ivanfslee@gmail.com>
2020-05-26 00:27:37 -0700

    Set headers for Content-Type and X-Powered-By. Also sent some JSON in the response using JSON.stringify().

---
+++ httpStatusCodes.js
@@ -0,0 +1,20 @@
+/*HTTP Status Codes
+
+1.xx Informational
+
+2.xx Success
+    200 Success
+    201 Created
+    204 No Content
+
+3.xx Redirection
+    304 Not Modified
+
+4.xx Client Error
+    400 Bad Request
+    401 Unauthorized
+    404 Not found
+
+5.xx Server Error
+    500 Internal Server Error
+*/
--- introNotes.js
+++ introNotes.js
@@ -31,22 +31,82 @@ What is HTTP?
 HTTP status codes:
     200 OK
     404 Not found
+
+npm install -D nodemon
+    -D stands for Development environment, as opposed to production
+
+    We only want nodemon installed as a dependency for our development environment
+
+    In your package.json - 'nodemon' will be listed under 'devDependencies' property
+
+'npm start' vs 'nodemon <file name>'
+
+    If you change the 'start' property in your 'scripts' property 
+    in your package.json from:
+
+        'start': 'node <file name>.js' 
+    
+    To:
+        'start': 'nodemon <file name>.js'
+
+    You can type 'npm start' to run your server.
+
+    If you don't change the 'start' property,
+        you can use:
+
+        'nodemon <file name>.js' to run your server instead
+    
+    The above method basically modifies the 'start' script
+
 */
 
 const http = require('http'); //http comes with node, so we don't have to 'npm install' it
+
+const todos = [
+    { id: 1, text: 'Todo One' },
+    { id: 2, text: 'Todo Two' },
+    { id: 3, text: 'Todo Three' }
+]
+
 
 //http has a method called 'createServer()' that takes a function
     //the function takes a req object and res object
 const server = http.createServer((req, res) => {
     
+    //We have to dictate what the content-type is being sent
+        //So the browser can formulate an appropriate response
+        //e.g.content-type: text/html, then browser renders the html
+
+    //In this course, we won't be sending HTML or templates
+        //Instead, our API will be responding with JSON
+    res.setHeader('Content-Type', 'application/json');
+
+    //X-Powered-By is not a standard header
+        //It specifies the technology supporting the web application
+    res.setHeader('X-Powered-By', 'Node.js');
+
+    //You can have multiple res.write()'s
+        // res.write('<h1>ALOHA</h1>');
+        // res.write('<h2>ALOHA AGAIN</h2>');
+
+    //If you are sending back one thing, you can put it in the res.end()
+
     //Using 'destructuring' JavaScript syntax to
         //pull out req.headers / req.url / req.method from req object
+            //const headers = req.headers;
+            //const url = req.url;
+            //const method = req.method;
     const { headers, url, method } = req;
 
     console.log(headers, url, method);
-    
+
     //res.end() will set status code as 200 (as long as there are no errors)
-    res.end(); 
+    //Note: JSON has double quotes around all the keys and values that are strings.
+        //So we have to JSON.stringify whwatever we want to send as part of the response
+    res.end(JSON.stringify({
+        success: true,
+        data: todos
+    }));
 });
 
 const PORT = 5000
